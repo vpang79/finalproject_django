111aada451ff31f3ead748ab94a7ced293663d67
Vincent Pang <vpang23@gmail.com>
2023-06-04 23:37:39 -0700

    Adding Login page

--- src/account/views.py
+++ src/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticateForm
 
 # Create your views here.
 def registration_view(request):
@@ -22,3 +22,32 @@ def registration_view(request):
         form = RegistrationForm()
         context['registration_form'] = form
     return render(request, 'account/register.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def login_view(request):
+    context={}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect("home")
+    
+    if request.POST:
+        form = AccountAuthenticateForm(request.POST)
+        if form.is_valid:
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AccountAuthenticateForm()
+
+    context['login_form'] = form
+    return render(request, 'account/login.html', context)
